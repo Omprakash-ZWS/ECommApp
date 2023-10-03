356c6e66293ea4bf767850003d2cb6a9476b3475
Omprakash-ZWS <omprakash.abhyankar@zobiwebsolutions.com>
2023-10-03 10:49:56 +0530

    Initial Commit

---
+++ EcommerceApplication/Models/ViewModel/CartDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApplication.Models.ViewModel
+{
+	public class CartDto
+	{
+		[Key]
+		public int ProductId { get; set; }
+
+		public string ProductName { get; set; }		
+        public int Total { get; set; }
+        public float Price { get; set; }
+		public int proquantity { get; set; }
+		public string SearchTerm { get; set; }
+		public int CategoryId { get; set; }
+		public Category category { get; set; }
+		public List<Category> categories { get; set; }
+	}
+}
---
+++ EcommerceApplication/Models/ViewModel/CategoryDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceApplication.Models.ViewModel
+{
+    public class CategoryDto    
+    {
+       
+		
+		public int CategoryId { get; set; }
+        
+        [Required]
+        public string CategoryName { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
+        [Required]
+        public string colour { get; set; }
+        [Required]
+        public string description { get; set; }
+
+       
+		public List<Category> categories { get; set; }
+
+
+	}
+  
+}
